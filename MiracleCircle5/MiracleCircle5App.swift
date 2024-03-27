ceff79066e0bab32821a09700efcedf9c65c6e37
윤범태 <yoonbumtae@gmail.com>
2024-03-28 01:07:35 +0900

    Build: create project with Fastlane integration

---
+++ MiracleCircle5/MiracleCircle5App.swift
@@ -0,0 +1,20 @@
+//
+//  MiracleCircle5App.swift
+//  MiracleCircle5
+//
+//  Created by 윤범태 on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MiracleCircle5App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
